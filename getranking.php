7b56263e5fbd23aa865886eacd939206ecfcf963
mmikulan <59137948+mmikulan@users.noreply.github.com>
2020-05-11 15:58:37 +0200

    Create getranking.php

---
+++ getranking.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once "credentials.php";
+require_once "functions.php";
+require_once "accesstoken.php";
+
+$teams = json_decode( file_get_contents( $nuligadatadir ."/nuliga_teams.json", true ));
+
+foreach ( $teams as $tid => $tdata ) {
+	$jsondata = getResource("/2014/federations/BHV/clubs/". $nuligateamid ."/teams/". $tid ."/table", $scope, array() );
+	$data = json_decode( $jsondata, true );
+
+	if ( ! isset( $output[ $tid ] )) { $output[ $tid ] = array(); }
+
+	foreach( $data['groupTable'] as $grp ) {
+		
+		$rr = (strpos( $grp['group'], "Rückrunde" ) > 0) || (strpos( $grp['group'], "RR ") > 0);
+		if ( isset( $output[ $tid ]['liga'] ) && ( ! $rr )) { continue; }
+		
+		$output[ $tid ]['liga'] = $grp['group'];
+		unset( $output[ $tid ]['tabelle'] );
+		foreach ( $grp['groupTableTeam'] as $gteam ) {
+			$output[ $tid ]['tabelle'][ $gteam['tableRank'] ] = array (
+				"team" 	 => $gteam['team'],
+				"own" 	 => ( $gteam['teamId'] == $tid ),
+				"matches" => $gteam['meetings'],
+				"ptsA" 	 => $gteam['ownPoints'],
+				"ptsB" 	 => $gteam['otherPoints'],
+				"goalsA" => $gteam['ownMatches'],
+				"goalsB" => $gteam['otherMatches'] );
+			// echo $gteam['otherPoints']
+			// echo $gteam['tableRank'] ." ";
+			// echo $gteam['team'] ." ";
+			// echo $gteam['ownPoints'] .":";
+			// echo $gteam['otherPoints'] ." ";
+			// echo $gteam['ownSets'] .":";
+			// echo $gteam['otherSets'] ." \n";
+		}
+	}
+}
+file_put_contents( $nuligawebdir ."/nuliga_ranking.json", json_encode( $output, JSON_PRETTY_PRINT ));
+?>
+
