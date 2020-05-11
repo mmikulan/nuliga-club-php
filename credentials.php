1556fca07841a146dbfca004b4d06752b6208d86
mmikulan <59137948+mmikulan@users.noreply.github.com>
2020-05-11 15:59:10 +0200

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -3,5 +3,5 @@ $clientid =     'bitte eintragen';
 $clientsecret = 'bitte eintragen';
 $scope = "club";
 $nuligateamid = 'bitte eintragen';
-$nuligawebdir = "Wordpress Verzeichnis wo die Daten gespeichert werden solle"; # zB. ../wp-website/nudata
+$nuligawebdir = '.' # Wordpress Verzeichnis wo die Daten gespeichert werden solle zB. ../wp-website/nudata
 ?>
